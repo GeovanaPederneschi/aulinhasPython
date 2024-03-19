25fef827637b9e61ae40ddf62a69ca489aa760d3
Geovana <geovanapederneschi@gmail.com>
2024-03-18 22:38:01 -0300

    Exercícios da aula 18_03

--- exercicios/ex1.py
+++ exercicios/ex1.py
@@ -1,4 +1,5 @@
 velocidade = int(input("Digite a velocidade do carro em km/h: "))
 if velocidade > 80:
-    valor = (velocidade - 80)*5
-    print("Usúario mutado em ", valor)
+    valor = (velocidade - 80)*5.00
+    print("Usúario mutado em R$", valor)
+    print(f"Usuário mutado em: R$ {valor:.2f}")
