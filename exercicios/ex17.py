a19908145aa128bcd5aa8b110ab5cdaaa37f9239
Geovana <geovanapederneschi@gmail.com>
2024-05-06 22:28:04 -0300

    commit

--- exercicios/ex17.py
+++ exercicios/ex17.py
@@ -1,11 +1,15 @@
-Lista_nome = []
+lista_nome = []
 print("Digite (s) para sair")
 while True:
     nome = str(input("Digite um nome: "))
     if nome == "s" or nome == "S":
         break
-    Lista_nome.append(nome)
+    lista_nome.append(nome)
+
+print(lista_nome)
+lista_nome.sort()
+
 x = 0
-while x < len(Lista_nome):
-    print(Lista_nome[x])
+while x < len(lista_nome):
+    print(lista_nome[x])
     x += 1
