6627a0d41594800dc5f0ad9dd837a2fa5ddc37f8
Geovana <geovanapederneschi@gmail.com>
2024-08-12 22:41:09 -0300

    12_08

--- exercicios/ex18.py
+++ exercicios/ex18.py
@@ -1,8 +1,8 @@
-cadastro = {"55586798958":["Tainara",1967,"Diadema"],
-            "55586798957":["Fernando",1987,"São João da Boa Vista"],
-            "55527425839":["Geovana",2005,"São Paulo"],
-            "36317871892":["Ana Clara",2004,"São Paulo"],
-            "47415915810":["Fernanda",2003,"São Paulo"]
+cadastro = {"55586798958": ["Tainara", 1967, "Diadema"],
+            "55586798957": ["Fernando", 1987, "São João da Boa Vista"],
+            "55527425839": ["Geovana", 2005, "São Paulo"],
+            "36317871892": ["Ana Clara", 2004, "São Paulo"],
+            "47415915810": ["Fernanda", 2003, "São Paulo"]
             }
 
 print(cadastro)
--- exercicios/ex19.py
+++ exercicios/ex19.py
@@ -1,9 +1,18 @@
-cadastro = {"55586798958":["Tainara",1967,"Diadema"],
-            "55586798957":["Fernando",1987,"São João da Boa Vista"],
-            "55527425839":["Geovana",2005,"São Paulo"],
-            "36317871892":["Ana Clara",2004,"São Paulo"],
-            "47415915810":["Fernanda",2003,"São Paulo"]
+cadastro = {"55586798958": ["Tainara", 1967, "Diadema", "SP"],
+            "55586798957": ["Fernando", 1987, "São João da Boa Vista", "SP"],
+            "55527425839": ["Geovana", 2005, "São Paulo", "SP"],
+            "36317871892": ["Ana Clara", 2004, "São Paulo", "SP"],
+            "47415915810": ["Fernanda", 2003, "São Paulo", "SP"]
             }
+
+for chave, valores in cadastro.items():
+    print()
+    print("Nome: ", valores[0])
+    print("CPF: ", chave)
+    print("Ano de Nascimento: ", valores[1])
+    print(f"Cidade e Estado de nascimento: , {valores[2]}/{valores[3]}\n")
+
+print()
 
 while True:
     cpf = input("Digite o cpf da pessoa: ").lower()
